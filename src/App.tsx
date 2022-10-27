f7381a8d8f2a4e29476d1c3c4e8b628ac573951e
oleg_torokhov <torokhov093@gmail.com>
2022-10-27 13:08:42 +0700

    выодим список задач

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import "./App.css";
 import { useGetTasksQuery } from "./api/todoApi";
+import { Task } from "./types";
 
 function App() {
-  const { data } = useGetTasksQuery();
-  console.log(data);
-  return <div className="App"></div>;
+  const { data, isLoading, isError, error } = useGetTasksQuery();
+
+  if (isLoading) {
+    return <div>Загружаем список задач</div>;
+  }
+
+  if (isError) {
+    return <div>{`Произошла ошибка ${error}`}</div>;
+  }
+
+  return (
+    <div>
+      <div>
+        {data?.map((task: Task) => (
+          <div key={task.id}>
+            <label>
+              <input type="checkbox" checked={task.completed} />
+              <span>{task.text}</span>
+            </label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
